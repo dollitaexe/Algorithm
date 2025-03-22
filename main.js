955e587a99f3d5e0343daee4d4da30b8e34597f5
dollitaexe <kamier.dalia909@gmail.com>
2025-03-22 22:56:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ let capital = ['Beijing','Tokyo','Seoul','Hanoï','Kuala Lumpur']
 for (let i = 0; i < capital.length; i++) {
     console.log("country:", countries[i],"capital:",capital[i])
 }
+let numberr = Math.floor(Math.random() * 4)
+if (numberr === 0) {
+  console.log("A certain victory")
+} else if (numberr === 1) {
+  console.log("not so certain victory")
+} else if (numberr === 2) {
+  console.log("an uneasy victory")
+} else {
+  console.log("Your fate is unclear, ô chosen undead")
+}
+
+function checkSeason(monthh) {
+  let m = monthh.toLowerCase()
+  if (m === 'december' || m === 'january' || m === 'february') return 'Winter'
+  if (m === 'march' || m === 'april' || m === 'may') return 'Spring'
+  if (m === 'june' || m === 'july' || m === 'august') return 'Summer'
+  if (m === 'september' || m === 'october' || m === 'november') return 'Autumn'
+  return 'No month right'
+}
