6334f6dd4b0e3872a3acb9b0267e70fd0032f041
dollitaexe <kamier.dalia909@gmail.com>
2025-03-22 22:41:38 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let message = "There is no war in Ba Sing Se"
+console.log(message.split(" ").length)
+let countries = ['China','Japan','South Korea','Vietnam','Malaysia']
+let capital = ['Beijing','Tokyo','Seoul','Hanoï','Kuala Lumpur']
+for (let i = 0; i < capital.length; i++) {
+    console.log("country:", countries[i],"capital:",capital[i])
+}
